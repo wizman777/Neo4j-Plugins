3612be1918a6d04ff9cf7533018defb7fb2f6e0a
Dima Kudriavcev <dmitrij@kudriavcev.info>
2016-07-23 18:32:46 +1000

    Removed unused imports

--- r-lookup-widget/src/main/java/org/researchgraph/neo4j/plugings/lookup/LookupService.java
+++ r-lookup-widget/src/main/java/org/researchgraph/neo4j/plugings/lookup/LookupService.java
@@ -1,9 +1,5 @@
 package org.researchgraph.neo4j.plugings.lookup;
 
-import static org.neo4j.server.rest.web.CustomStatusType.UNPROCESSABLE;
-
-import java.io.IOException;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -12,18 +8,11 @@ import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.StreamingOutput;
 
-import org.codehaus.jackson.JsonEncoding;
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.neo4j.cypher.CypherException;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.helpers.collection.MapUtil;
@@ -32,7 +21,6 @@ import org.neo4j.server.rest.repr.CypherResultRepresentation;
 import org.neo4j.server.rest.repr.ExceptionRepresentation;
 import org.neo4j.server.rest.repr.InputFormat;
 import org.neo4j.server.rest.repr.OutputFormat;
-import org.neo4j.server.rest.repr.Representation;
 import org.neo4j.server.rest.transactional.error.Neo4jError;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
@@ -40,7 +28,6 @@ import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 @Path("/lookup")
 public class LookupService {
 	private GraphDatabaseService graphDb;
-	private final ObjectMapper objectMapper;    
 	private final InputFormat input;
     private final OutputFormat output;
     
@@ -50,7 +37,6 @@ public class LookupService {
 			@Context InputFormat input, @Context OutputFormat output )
 	{
 		this.graphDb = graphDb;
-		this.objectMapper = new ObjectMapper();
 		this.input = input;
 		this.output = output;
 	}
